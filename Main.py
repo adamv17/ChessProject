bf9d08e823e59ef3e703ca63f24af0a98cd2ee85
Adam <adam.vichik@gmail.com>
2022-02-01 09:30:28 +0200

    Disable multi touch
    No more red dots!! yay

--- Main.py
+++ Main.py
@@ -1,5 +1,7 @@
 from ChessApp import ChessApp
+from kivy.config import Config
 
 if __name__ == '__main__':
     print("Chess is awesome")
+    Config.set('input', 'mouse', 'mouse,disable_multitouch')
     ChessApp().run()
