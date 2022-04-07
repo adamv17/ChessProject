e2a7d6c5984e1a7a25785aee8fcd228e73bc2e27
Adam <adam.vichik@gmail.com>
2022-04-07 13:07:33 +0300

    Some new models

--- Main.py
+++ Main.py
@@ -1,7 +1,10 @@
 from ChessApp import ChessApp
 from kivy.config import Config
+from kivy.core.window import Window
 
 if __name__ == '__main__':
     print("Chess is awesome")
     Config.set('input', 'mouse', 'mouse,disable_multitouch')
     ChessApp().run()
+
+
